3d0b2957970c71a023d55d0436681f6aeaed6041
Pongpob Chuennanthachai <116894250+PongpobCH@users.noreply.github.com>
2025-12-21 16:46:40 +0700

    WaterandElectric
    Update: Add Water And Electric team

--- scriptforcharacterpage.js
+++ scriptforcharacterpage.js
@@ -5,3 +5,36 @@ springfieldbutton.addEventListener("click", tosf)
 function tosf(){
     window.location.replace('./Springfield.html');
 }
+
+const florencebutton = document.getElementById('florence')
+
+florencebutton.addEventListener("click", toflorence)
+
+function toflorence(){
+    window.location.replace('./florence.html');
+}
+
+const tololobutton = document.getElementById('tll')
+
+tololobutton.addEventListener("click", totll)
+
+function totll(){
+    window.location.replace('./tololo.html');
+}
+
+const Nikketabutton = document.getElementById('nikketa')
+
+Nikketabutton.addEventListener("click", nik)
+
+function nik(){
+    window.location.replace('./Nikketa.html');
+}
+
+const Levabutton = document.getElementById('leva')
+
+Levabutton.addEventListener("click", leva)
+
+function leva(){
+    window.location.replace('./leva.html');
+}
+
